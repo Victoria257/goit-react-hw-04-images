496689e498ccd4ba706aea04f503f59ce9ed5256
Viktoria <vik25@i.ua>
2023-03-23 20:02:27 +0100

    refactoring modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,33 +1,30 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  static propTypes = {
-    onCloseModal: PropTypes.func.isRequired,
-    onPressEsc: PropTypes.func.isRequired,
-    largeImage: PropTypes.string.isRequired,
-  };
+export function Modal({ onCloseModal, largeImage, onPressEsc }) {
+  useEffect(() => {
+    window.addEventListener('keydown', onPressEsc);
+    return () => {
+      window.removeEventListener('keydown', onPressEsc);
+    };
+  }, [onPressEsc]);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.props.onPressEsc);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.props.onPressEsc);
-  }
-
-  render() {
-    return createPortal(
-      <div className={css.overlay} onClick={this.props.onCloseModal}>
-        <div className={css.modal}>
-          <img name="imageBig" src={this.props.largeImage} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={css.overlay} onClick={onCloseModal}>
+      <div className={css.modal}>
+        <img name="imageBig" src={largeImage} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
+
+Modal.propTypes = {
+  onCloseModal: PropTypes.func.isRequired,
+  onPressEsc: PropTypes.func.isRequired,
+  largeImage: PropTypes.string.isRequired,
+};
